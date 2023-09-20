9a7c41e7259ce6a1f2be917379099b41feec2aec
Jackson Salopek <jackson.salopek@gmail.com>
2023-09-19 23:18:24 -0400

    feat: added example

---
+++ example/src/index.ts
@@ -0,0 +1,16 @@
+import { Aponia } from "aponia/src";
+import { performance } from "perf_hooks";
+
+const start = performance.now();
+const app = new Aponia({ basePath: "api" });
+
+await app.start().then(
+  (instance) => {
+    const end = performance.now();
+    const timeToStart = end - start;
+    console.log(
+      `🐎 Aponia started: ${instance.server?.hostname}:${instance.server?.port} (${timeToStart}ms)`
+    );
+  },
+  (reason) => console.error(`Couldn't boostrap Aponia!\nreason: ${reason}`)
+);
